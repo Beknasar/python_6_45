928e06db518665bb2c32551b60d29af39cff4e98
Beknasar <680633@gmail.com>
2020-07-23 13:41:38 +0600

    Добавлена страница со списком статей.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,38 +1,21 @@
 from django.shortcuts import render
-
+from webapp.models import Article
 # Create your views here.
 def index_view(request):
+    data = Article.objects.all()
+    return render(request, 'index.html', context={
+        'articles': data
+    })
+
+def article_create_view(request):
     if request.method == 'GET':
-        return render(request, 'index.html')
+        return render(request, 'article_create.html')
     elif request.method == 'POST':
+        print(request.POST)
         context = {
-            'number_1': int(request.POST.get('number_1', 0)),
-            'number_2': int(request.POST.get('number_2', 0)),
+            'title': request.POST.get('title'),
+            'content': request.POST.get('content'),
+            'author': request.POST.get('author')
         }
-        if request.POST.get('operation') == 'plus':
-            result = context['number_1'] + context['number_2']
-            text = {
-                'operation': '+',
-                'result': result
-            }
-            context.update(text)
-        elif request.POST.get('operation') == 'minus':
-            result = context['number_1'] - context['number_2']
-            text = {
-                'operation': '-',
-                'result': result
-            }
-        elif request.POST.get('operation') == 'multiply':
-            result = context['number_1'] * context['number_2']
-            text = {
-                'operation': '*',
-                'result': result
-            }
-        else:
-            result = context['number_1'] / context['number_2']
-            text = {
-                'operation': '/',
-                'result': result
-            }
-        context.update(text)
-        return render(request, 'index.html', context)
+
+        return render(request, 'article_view.html', context)
