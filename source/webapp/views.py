7be11e451c922816fa01c9fd4923662843446d24
Beknasar <680633@gmail.com>
2020-07-23 17:20:13 +0600

    Добавлена добавление статьи на сайт.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 from webapp.models import Article
-# Create your views here.
+from django.http import HttpResponseNotAllowed
+
+
 def index_view(request):
     data = Article.objects.all()
     return render(request, 'index.html', context={
-        'articles': data
-    })
+        'articles': data })
 
 def article_create_view(request):
     if request.method == 'GET':
         return render(request, 'article_create.html')
     elif request.method == 'POST':
-        print(request.POST)
-        context = {
-            'title': request.POST.get('title'),
-            'content': request.POST.get('content'),
-            'author': request.POST.get('author')
-        }
+        #print(request.POST)
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        author = request.POST.get('author')
+        article = Article.objects.create(title=title,
+                                         text=text,
+                                         author=author)
+        context = {'article': article}
 
         return render(request, 'article_view.html', context)
+    else:
+        return HttpResponseNotAllowed(
+            permitted_methods=['GET', 'POST'])
