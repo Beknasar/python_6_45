16883869cff9a2e5c948103185e44cd9578aca63
Beknasar <680633@gmail.com>
2020-07-23 18:15:08 +0600

    Добавлено поле статус с вариантами выбора в статью и его вывод в форме добавления и страница списка стаей.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render
-from webapp.models import Article
+from webapp.models import Article, STATUS_CHOICES
 from django.http import HttpResponseNotAllowed
 
 
 def index_view(request):
-    data = Article.objects.all()
+    is_admin = request.GET.get("is_admin", None)
+    if is_admin:
+        data = Article.objects.all()
+    else:
+        data = Article.objects.filter(status='moderated')
     return render(request, 'index.html', context={
         'articles': data })
 
 def article_create_view(request):
     if request.method == 'GET':
-        return render(request, 'article_create.html')
+        return render(request, 'article_create.html', context={
+            'status_choices': STATUS_CHOICES
+        })
     elif request.method == 'POST':
         #print(request.POST)
         title = request.POST.get('title')
         text = request.POST.get('text')
+        status = request.POST.get('status')
         author = request.POST.get('author')
         article = Article.objects.create(title=title,
                                          text=text,
-                                         author=author)
+                                         author=author,
+                                         status=status)
         context = {'article': article}
 
         return render(request, 'article_view.html', context)
